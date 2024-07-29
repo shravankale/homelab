8c2a3da565f2d91b7b18a4fe8e3b28aca716471b
Shravan Kale <shravan.kale@gmail.com>
2024-07-29 19:51:47 +0530

    Updated generate_configs.py to handle a single template and a directory full of template files

--- utils/generate_configs.py
+++ utils/generate_configs.py
@@ -5,74 +5,91 @@ Usage: Check "python generate_configs.py --help """
 import os, re
 import yaml, argparse
 
-def generate_configs_from_templates(template_dir, config_dir, env_file):
-    
-    #Get Secrets
+#Using a regex for identifying env vars or secrets in template files
+REGEX = re.compile(r'{{\s*([\w_]+)\s*}}|\$\{\s*([\w_]+)\s*}')
+
+def get_secrets(env):
+
     secrets = {}
-    if os.path.isfile(env_file):
-        if env_file.split('.')!='env':
-            print(f"The {{env_file}} is not a .env environment file")
-        with open(env_file, 'r') as file:
+    if os.path.isfile(env):
+        if env.split('.')!='env':
+            print(f"The {{env}} is not a .env environment file")
+        with open(env, 'r') as file:
             for line in file:
                 key,value = line.strip().split('=')
                 secrets[key]=value
-    elif os.path.isdir(env_file):
-        for file in os.listdir(env_file):
+    elif os.path.isdir(env):
+        for file in os.listdir(env):
             if file.split('.')[-1]=='env':
-                with open(os.path.join(env_file,file), 'r') as file:
+                with open(os.path.join(env,file), 'r') as file:
                     for line in file:
                         key,value = line.strip().split('=')
                         secrets[key]=value
     else:
-        if not os.path.exists(env_file):
-            print(f"Path {{env_file}}: does not exist")
+        if not os.path.exists(env):
+            print(f"Path {{env}}: does not exist")
         else:
-            print(f"Path {{env_file}}: exists but is not a directory or file")
+            print(f"Path {{env}}: exists but is not a directory or file")
 
     if len(secrets)==0:
         print("No environment files found")
-    
-    #Using a regex for identifying env vars or secrets in template files
-    regex = re.compile(r'{{\s*([\w_]+)\s*}}|\$\{\s*([\w_]+)\s*}')
+
+    return secrets
+
+def template_to_config(file, file_dir, config_dir,secrets):
+
+    if file.split('.')[-2]=='template':
+        template_path = os.path.join(file_dir,file)
+
+        # Extract the base filename without the last extension
+        config_filename = file.split('.')[0]+'.yaml'
+        config_path = os.path.join(config_dir, config_filename)
+
+        #Replace placeholders with values from secrets
+        modified_config = []
+        with open(template_path,'r') as template_file: #, open(config_path,'w') as config_file:
+            for line in template_file:
+                match = REGEX.search(line)
+                if match:
+                    if match.group(1):
+                        key = match.group(1)
+                    else:
+                        key = match.group(2)
+                    #.get(key,default) is useful in case of errors such as missing values
+                    line = line.replace(match.group(0), secrets.get(key, match.group(0)))
+                    modified_config.append(line)
+                else:
+                    modified_config.append(line)
+            
+        with open(config_path,'w') as config_file:
+            config_file.writelines(modified_config)
+
+def generate_configs_from_templates(template, config_dir, secrets):
     
     #Go through template files and generate config files
-    for file in os.listdir(template_dir):
-
-        if file.split('.')[-2]=='template':
-            template_path = os.path.join(template_dir,file)
-
-            # Extract the base filename without the last extension
-            config_filename = file.split('.')[0]+'.yaml'
-            config_path = os.path.join(config_dir, config_filename)
-
-            #Replace placeholders with values from secrets
-            modified_config = []
-            with open(template_path,'r') as template_file: #, open(config_path,'w') as config_file:
-                for line in template_file:
-                    match = regex.search(line)
-                    if match:
-                        if match.group(1):
-                            key = match.group(1)
-                        else:
-                            key = match.group(2)
-                        #.get(key,default) is useful in case of errors such as missing values
-                        line = line.replace(match.group(0), secrets.get(key, match.group(0)))
-                        modified_config.append(line)
-                    else:
-                        modified_config.append(line)
-                
-            with open(config_path,'w') as config_file:
-                config_file.writelines(modified_config)
+    if os.path.isfile(template):
+        file = os.path.basename(template)
+        file_dir = os.path.dirname(template)
+        template_to_config(file,file_dir,config_dir,secrets)
+    elif os.path.isdir(template):
+        for file in os.listdir(template):
+            template_to_config(file,template,config_dir,secrets)
+    else:
+        print(f"{template} as a file or directory does not exist")
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--from-templates', action="store_true", help="Generate configs from templates")
-    parser.add_argument('--template-dir', type=str, help="Path to templates directory with *.yaml.template files")
+    parser.add_argument('--template', type=str, help="Path to templates directory with *.template.yaml files or a single file")
     parser.add_argument('--config-dir', type=str, help="Path to output configuration directory for a given service")
-    parser.add_argument('--using-env',type=str, help="Path to a secrets environment file")
+    parser.add_argument('--using-env',type=str, help="Path to a secrets environment file or a directory with .env files")
 
     args = parser.parse_args()
 
     if args.from_templates:
-        generate_configs_from_templates(args.template_dir, args.config_dir, args.using_env)
+        #Get Secrets
+        secrets = get_secrets(args.using_env)
+        generate_configs_from_templates(args.template,args.config_dir, secrets)
+
+# --from-templates --template utils/sample_config.template.yaml --config-dir utils --using-env utils
